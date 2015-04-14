50d56db6df547ca7a1fbd00a271d6be459ca72b7
ozit <ozit@free.fr>
2015-04-14 19:12:31 +0200

    accents dans les citations

--- custom/plugins/motd/motd.plugin.zsh
+++ custom/plugins/motd/motd.plugin.zsh
@@ -11,4 +11,4 @@
 echo "${fg[${ZSH_HOST_COLOR}]}$(uptime)"
 echo "Kernel: $(uname -r) ($(uname -v))${reset_color}"
 echo ""
-[ -f /bin/fortune ] && fortune "${ZSH_FORTUNE_ARG:-}" && echo ""
+[ -f /bin/fortune ] && fortune "${ZSH_FORTUNE_ARG:-}" | iconv -c --silent -f ISO_8859-1 -t UTF-8 | fold -s -w 80 && echo ""
